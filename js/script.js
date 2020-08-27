117976823955eab958df95c6b7ab848424cba4d5
Fabrizio <fabriziospoto1@gmail.com>
2020-08-27 16:11:06 +0200

    Griglia
    Griglia

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+// Generare una griglia 6x6 (36 boxes), ad ogni click parte una richiesta
+// AJAX che prende un numero random da 1 a 9 (scegliere API opportuna).
+// Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
+// Il numero ottenuto appare al centro del quadrato.
+
 $( document ).ready(function() {
-    alert('ciao');
+    //Ciclo for per clonazione quadrati
+    for (var i = 0; i < 35; i++) {
+        $('.container').append('<div class="square"></div>');
+    }
+
 });
